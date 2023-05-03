f6c10942ee1f61c6c455438d66f0e867fb666d57
BSalaheddine <195159@ecam.be>
2023-05-03 13:43:04 +0200

    update aruco detection

--- suitcase/src/test_video_publisher.py
+++ suitcase/src/test_video_publisher.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
-# Basics ROS program to publish real-time streaming
-# video from your built-in webcam
+# Basic ROS program to publish real-time streaming
+# video from your built-in webcam and detect an ArUco tag with the ID 667
 # Author:
 # - Addison Sears-Collins
 # - https://automaticaddison.com
-# Modified to detect ArUco tag with ID 667 and stream video over the network
 
 # Import the necessary libraries
 import rospy  # Python library for ROS
-from sensor_msgs.msg import CompressedImage  # CompressedImage is the message type
+from sensor_msgs.msg import Image  # Image is the message type
 from std_msgs.msg import Int16  # Image is the message type
 import cv2  # OpenCV library
+from cv_bridge import CvBridge  # Package to convert between ROS and OpenCV Images
 import numpy as np
 import math
 
-def publish_message():
+# Import ArUco module
+import cv2.aruco as aruco
 
+
+def publish_message():
     # Node is publishing to the video_frames topic using
-    # the message type CompressedImage
-    pub = rospy.Publisher('video_frames/compressed', CompressedImage, queue_size=10)
+    # the message type Image
+    pub = rospy.Publisher('video_frames', Image, queue_size=10)
     pub2 = rospy.Publisher('pixel', Int16, queue_size=10)
     # Tells rospy the name of the node.
     # Anonymous = True makes sure the node has a unique name. Random
@@ -33,10 +36,13 @@ def publish_message():
     cap = cv2.VideoCapture(0)
     WIDTH = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     HEIGHT = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    
-    # Load the ArUco dictionary and parameters
-    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-    aruco_params = cv2.aruco.DetectorParameters_create()
+
+    # Used to convert between ROS and OpenCV images
+    br = CvBridge()
+
+    # Create ArUco dictionary
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    aruco_parameters = aruco.DetectorParameters_create()
 
     # While ROS is still running.
     while not rospy.is_shutdown():
@@ -51,43 +57,48 @@ def publish_message():
             rospy.loginfo('publishing video frame')
 
             # Detect ArUco markers
-            corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(frame, aruco_dict, parameters=aruco_params)
-
-            # Draw detected markers
-            frame = cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=aruco_parameters)
 
             if ids is not None:
-                # Find the ArUco tag with ID 667
-                for i, tag_id in enumerate(ids.flatten()):
-                    if tag_id == 667:
-                        # Get the center of the detected ArUco tag
+                for i in range(len(ids)):
+                    if ids[i] == 667:
+                        # Draw a rectangle around the ArUco marker
+                        cv2.polylines(frame, [np.int32(corners[i])], True, (0, 255, 0), 3)
+
+                        # Calculate the center of the ArUco marker
                         center = tuple(np.mean(corners[i][0], axis=0).astype(int))
+                        cv2.circle(frame, center, 1, (255, 0, 0), 2)
 
-                        # Draw a circle at the center of the ArUco tag
-                        cv2.circle(frame, center, 3, (0, 0, 255), 2)
+                        # Draw a line from the center of the ArUco marker to the center of the camera
+                        camera_center = (frame.shape[1]//2, frame.shape[0]//2)
+                        cv2.line(frame, center, camera_center, (255, 0, 0), 2)
 
-                        # Calculate the x-coordinate of the center of the rectangle relative to the center of the screen
-                        camera_center = (frame.shape[1] // 2, frame.shape[0] // 2)
+                        # Calculate the angle between the line connecting the center of the ArUco marker and the center of the camera and the vertical line through the middle of the camera
+                        dx = camera_center[0] - center[0]
+                        dy = camera_center[1] - center[1]
+                        angle = math.degrees(math.atan2(dy, dx))
+                        cv2.putText(frame, f"Angle: {angle:.2f}", (20, 40),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+                        # Calculate the x-coordinate of the center of the ArUco marker relative to the center of the screen
                         x_offset = center[0] - camera_center[0]
+                        cv2.putText(frame, f"X-Offset: {x_offset:.2f}", (20, 80),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
-                        # Publish the x_offset
-                        pub2.publish(x_offset)
+            # Publish the image.
+            # The 'cv2_to_imgmsg' method converts an OpenCV
+            # image to a ROS image message
+            pub.publish(br.cv2_to_imgmsg(frame, encoding='rgb8'))
+            pub2.publish(x_offset)
 
-            # Encode the frame as a compressed JPEG image
-            compressed_frame = cv2.imencode('.jpg', frame)[1].tobytes()
+            # Sleep just enough to maintain the desired rate
+            rate.sleep()
 
-            # Create a CompressedImage message and publish the encoded frame
-            msg = CompressedImage()
-            msg.header.stamp = rospy.Time.now()
-            msg.format = "jpeg"
-            msg.data = compressed_frame
-            pub.publish(msg)
-
-        # Sleep just enough to maintain the desired rate
-        rate.sleep()
 
 if __name__ == '__main__':
     try:
         publish_message()
     except rospy.ROSInterruptException:
         pass
+
