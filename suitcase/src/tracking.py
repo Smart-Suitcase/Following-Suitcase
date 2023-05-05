f7343e43e12f6d9822f2a4f77428bd83e9061a6d
Eliott Nys <eliott.nys@gmail.com>
2023-05-05 14:42:20 +0200

    added tracking test files

---
+++ suitcase/src/tracking.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+
+# Define the lower and upper boundaries of the object's color in the HSV color space
+# You can adjust these values depending on the color of the object you want to track
+# Proposed lower bound: [154.18413978  38.7188172   36.36586022]
+# Proposed upper bound: [174.18413978 138.7188172  136.36586022]
+lower_color = np.array([149.18413978, 18.7188172, 16.36586022])
+upper_color = np.array([179.18413978, 158.7188172, 156.36586022])
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+
+# Start a loop to continuously capture frames from the webcam
+while True:
+    # Capture a frame
+    ret, frame = cap.read()
+    
+    # Convert the frame to the HSV color space
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    # Create a mask using the lower and upper boundaries of the object's color
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+    
+    # Find the contours of the object in the mask
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # If any contours are found, draw a bounding box around the largest one
+    if len(contours) > 0:
+        # Find the largest contour
+        largest_contour = max(contours, key=cv2.contourArea)
+        # Get the bounding box coordinates of the largest contour
+        x, y, w, h = cv2.boundingRect(largest_contour)
+        # Draw the bounding box around the object in the original frame
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    # Display the original frame and the mask
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    
+    # Wait for a key press and check if it's the escape key
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+# Release the webcam and close the windows
+cap.release()
+cv2.destroyAllWindows()
